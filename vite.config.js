40a96abfc231a1a5f01fb59e3fd0207e65b888b6
Vinicius Cardoso Garcia <vcg@cin.ufpe.br>
2025-12-01 18:51:06 -0300

    Chore: Configura base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bisolhador/', // <--- ADICIONE ISSO (Nome do repositório entre barras)
 })
