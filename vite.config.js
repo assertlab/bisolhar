e953a0e87b75376c40d73b3d3d6c94dfc5c711c8
Vinicius Cardoso Garcia <vcg@cin.ufpe.br>
2025-12-01 18:58:08 -0300

    Fix(deploy): Define base path correto para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bisolhador/', // <--- ADICIONE ISSO (Nome do repositório entre barras)
+  base: '/bisolhador/', // <--- ADICIONE ESTA LINHA IMPORTANTE
 })
